ada5b58e2a8b5b44f53527ac65091037305470a5
Alessandro <ale-remo@hotmail.com>
2016-03-30 02:47:34 -0300

    Class BusData added

---
+++ app/src/main/java/com/example/android/bustracker/BusData.java
@@ -0,0 +1,11 @@
+package com.example.android.bustracker;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Alessandro on 27/03/2016.
+ */
+public class BusData {
+    private ArrayList<String> listOfBuses = new ArrayList<>();
+
+}
