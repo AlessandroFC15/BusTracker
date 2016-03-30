bcc5be5a1a52b03924665e523d4a066bf51ccbaa
Alessandro <ale-remo@hotmail.com>
2016-03-30 18:35:23 -0300

    Add ride feature added

--- app/src/main/java/com/example/android/bustracker/AddRide.java
+++ app/src/main/java/com/example/android/bustracker/AddRide.java
@@ -1,8 +1,7 @@
 package com.example.android.bustracker;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.AdapterView;
@@ -10,24 +9,25 @@ import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 public class AddRide extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     // Attributes
-    private ArrayList<String> listOfBuses = new ArrayList<String>();
+    private BusData busData = BusData.getInstance();
+    private Set<String> nameOfBuses = busData.getListOfBuses().keySet();
+    private String busSelected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_ride);
 
-        Intent intent = getIntent();
-
-        listOfBuses = (ArrayList) intent.getSerializableExtra(MainActivity.LIST_BUSES);
-
-        if (listOfBuses.isEmpty()) {
+        if (nameOfBuses.isEmpty()) {
             printWarning();
         } else {
             setSpinner();
@@ -37,12 +37,19 @@ public class AddRide extends AppCompatActivity implements AdapterView.OnItemSele
     // Methods related to the spinner
 
     private void setSpinner() {
+
+        // Store all the names of buses in a list to use in the ArrayAdapter
+        List<String> listOfBuses = new ArrayList<>(nameOfBuses);
+
         Spinner spinner = (Spinner) findViewById(R.id.busSpinner);
+
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_spinner_item, listOfBuses);
+
         // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
 
@@ -55,14 +62,13 @@ public class AddRide extends AppCompatActivity implements AdapterView.OnItemSele
         // parent.getItemAtPosition(pos)
         String item = parent.getItemAtPosition(pos).toString();
 
-        //grade = item;
+        busSelected = item;
     }
 
     public void onNothingSelected(AdapterView<?> parent) {
         // Another interface callback
-        //grade = "EXC";
+        busSelected = "";
     }
-
 
     private void printWarning() {
         clearScreen();
@@ -77,11 +83,35 @@ public class AddRide extends AppCompatActivity implements AdapterView.OnItemSele
 
         // Add the warning just after the heading.
         mainLayout.addView(warning);
-
     }
 
     private void clearScreen() {
         LinearLayout layout = (LinearLayout) findViewById(R.id.inputNewRide);
         layout.removeAllViews();
+    }
+
+    public void addRide(View view)
+    {
+        // Validate bus selection
+        if (busSelected.equals(""))
+        {
+            makeToast("Select a bus!");
+            return ;
+        }
+        // We proceed to add a ride for that bus
+        if (busData.addRide(busSelected) == false)
+        {
+            makeToast("Operation failed. Try again!");
+        } else
+        {
+            makeToast("Ride for bus " + busSelected + " added!");
+        }
+
+    }
+
+    private void makeToast(String text)
+    {
+        Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
